8fc74398f49c4a3945dc08d00b55a974f49b4cb7
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-06 13:38:52 -0400

    up to 3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyles from "../GlobalStyles";
 
 const App = () => {
-  return <Router>TODO</Router>;
+  return (
+    <Router>
+      DAJE ROMA! <GlobalStyles />
+    </Router>
+  );
 };
 
 export default App;
