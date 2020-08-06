525b4306100e4559a708fcabc94621f3c1d4ac7d
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-06 14:50:17 -0400

    done to 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import GlobalStyles from "../GlobalStyles";
+import ArtistRoute from "../ArtistRoute/ArtistRoute";
+const DEFAULT_ARTIST_ID = "4RnBFZRiMLRyZy0AzzTg2C";
 
 const App = () => {
   return (
     <Router>
-      DAJE ROMA! <GlobalStyles />
+      <Switch exact path="/">
+        Daje Roma
+      </Switch>
+      <Switch>
+        <Route path="/artist/:artistId">
+          <ArtistRoute />
+        </Route>
+        <Redirect to={`/artist/${DEFAULT_ARTIST_ID}`} />
+      </Switch>
+
+      <GlobalStyles />
     </Router>
   );
 };
---
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ArtistRoute extends Component {
+  render() {
+    return <div>Artist go here </div>;
+  }
+}
+
+export default ArtistRoute;
