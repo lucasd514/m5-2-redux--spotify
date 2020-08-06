48651688c9530ef6766e730ce29407525728405c
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-06 14:04:52 -0400

    q4

---
+++ src/reducers/artist-reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  token: null,
+  status: "idle",
+};
+
+export default function artists(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
